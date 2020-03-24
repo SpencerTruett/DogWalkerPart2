0311287cfd3756213199625e99065c1f3c1e0a4a
Spencer Truett <srtruett1993@gmail.com>
2020-03-24 13:39:14 -0500

    first commit

---
+++ PawsitivelyBestDogWalkerPart2/Data/WalkerRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PawsitivelyBestDogWalkerPart2.Data
+{
+    class WalkerRepository
+    {
+    }
+}
---
+++ PawsitivelyBestDogWalkerPart2/Models/Owner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PawsitivelyBestDogWalkerPart2.Models
+{
+    class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int NeighborhoodId { get; set; }
+        public Neighborhood Neighborhood { get; set; }
+        public string Phone { get; set; }
+    }
+}
